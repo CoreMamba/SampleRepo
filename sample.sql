5432fda165747e1b48fc3c33362562c21338b8f0
CoreMamba <215352410+CoreMamba@users.noreply.github.com>
2025-07-16 16:52:46 +0000

    add

---
+++ sample.sql
@@ -0,0 +1,16 @@
+-- SHOW DATABASES;
+-- USE mysql;
+-- USE sampledb;
+-- Create Table if not EXISTS customers (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     first_name VARCHAR(50) NOT NULL,
+--     last_name VARCHAR(50) NOT NULL,
+--     email VARCHAR(100) NOT NULL UNIQUE,
+--     phone VARCHAR(15),
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- DESCRIBE customers;
+-- SELECT * FROM sampledb.customers;
+-- SHOW TABLES FROM sampledb;
+-- SHOW TABLES;
