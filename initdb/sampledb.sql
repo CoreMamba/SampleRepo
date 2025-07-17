47a84a8b858cab92386b865cdfd2068b33281474
CoreMamba <215352410+CoreMamba@users.noreply.github.com>
2025-07-17 17:42:11 +0000

    Update: weekly_sales_sheet table changes in salesdatabase

--- initdb/sampledb.sql
+++ initdb/sampledb.sql
@@ -31,7 +31,7 @@ CREATE TABLE `customers` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,6 +40,7 @@ CREATE TABLE `customers` (
 
 LOCK TABLES `customers` WRITE;
 /*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES (1,'John','Doe','john.doe@example.com','123-456-7890','2025-07-17 17:32:32'),(2,'Jane','Smith','jane.smith@example.com','987-654-3210','2025-07-17 17:32:32');
 /*!40000 ALTER TABLE `customers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-16 16:58:19
+-- Dump completed on 2025-07-17 17:41:25
